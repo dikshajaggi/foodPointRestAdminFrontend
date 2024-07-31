12fdd8fba9c2bee22c11777f3bbee24d824a1355
cee-diksha <cee.diksha@gmail.com>
2024-07-31 16:45:47 +0530

    basic setup done

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const login = () => {
   return (
     <View className="flex justify-center items-center h-full w-full">
       <Text>helloo loginnn</Text>
+      <Link href="/home">Go to home</Link>
     </View>
   )
 }
